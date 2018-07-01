ad4bb749bf65c24c3c0ecb40f8eaf0d6354484ee
ariel tapia <atapia@sparkdigital.com>
2018-07-01 15:28:14 -0300

    first commit

---
+++ dist/static/NoMatch.724f211e7295b5477251.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{175:function(e,n,t){"use strict";Object.defineProperty(n,"__esModule",{value:!0});var u=o(t(0)),l=t(55),a=o(t(73));function o(e){return e&&e.__esModule?e:{default:e}}n.default=function(){return u.default.createElement(a.default,null,u.default.createElement(l.Icon,{name:"minus circle",size:"big"}),u.default.createElement("strong",null,"Page not found!"))}}}]);
+//# sourceMappingURL=NoMatch.724f211e7295b5477251.js.map
